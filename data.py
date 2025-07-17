backprop fix + viz

data.py
@@ -7,6 +7,7 @@ import torch
 from loguru import logger
 from omegaconf import OmegaConf
 from torch import Tensor, nn
+from torch.nn.functional import normalize
 from torch.nn.parameter import Parameter
 from torch.utils.data import DataLoader
 from torch_geometric.data import Data
@@ -425,7 +426,13 @@ class GraphGenerator(ModelInspector):
             gradients: Tensor = self.flatten_and_zero_grad()
 
             # dim = [num_vertices, num_features] 
+            in_feature = self.mean_var_nomralize(in_feature)
+            out_feature = self.mean_var_nomralize(out_feature)
+            weight_feature = self.mean_var_nomralize(self.weight_feature)
+            gradient_feature = self.mean_var_nomralize(gradients)
+
             graph_feature_matrix = torch.column_stack((in_feature, out_feature, self.weight_feature, gradients))
+            # logger.info(f'Vector norm {torch.linalg.vector_norm(graph_feature_matrix[0])}, {torch.linalg.vector_norm(graph_feature_matrix[1])}, {torch.linalg.vector_norm(graph_feature_matrix[2])}, {torch.linalg.vector_norm(graph_feature_matrix[3])}')
             
             # # tracer
             # logger.info(f'{in_feature.shape} | {out_feature.shape} | {self.weight_feature.shape} | {gradients.shape}')
@@ -470,6 +477,9 @@ class GraphGenerator(ModelInspector):
             del feature_batch, input_batch, target_batch
             self.model.reset_activations()
             torch.cuda.empty_cache()
+    
+    def mean_var_nomralize(self, feature_vector: Tensor) -> Tensor:
+        return (feature_vector - feature_vector.mean()) / (feature_vector.std() + 1e-8)
 
     def get_data(self, idx: int=0) -> Tuple[Any, Any, Data]:
         """Generate target and PyG graph data"""
@@ -494,7 +504,12 @@ class GraphGenerator(ModelInspector):
                 gradients: Tensor = self.flatten_and_zero_grad()
 
                 # dim = [num_vertices, num_features]
-                graph_feature_matrix = torch.column_stack((in_feature, out_feature, self.weight_feature, gradients))
+                in_feature = self.mean_var_nomralize(in_feature)
+                out_feature = self.mean_var_nomralize(out_feature)
+                weight_feature = self.mean_var_nomralize(self.weight_feature)
+                gradient_feature = self.mean_var_nomralize(gradients)
+
+                graph_feature_matrix = torch.column_stack((in_feature, out_feature, weight_feature, gradient_feature))
                 
                 # # tracer
                 # logger.info(f'{in_feature.shape} | {out_feature.shape} | {self.weight_feature.shape} | {gradients.shape}')
@@ -531,8 +546,12 @@ class GraphGenerator(ModelInspector):
             gradients: Tensor = self.flatten_and_zero_grad()
 
             # dim = [num_vertices, num_features] 
-            graph_feature_matrix = torch.column_stack((in_feature, out_feature, self.weight_feature, gradients))
-            
+            in_feature = self.mean_var_nomralize(in_feature)
+            out_feature = self.mean_var_nomralize(out_feature)
+            weight_feature = self.mean_var_nomralize(self.weight_feature)
+            gradient_feature = self.mean_var_nomralize(gradients)
+
+            graph_feature_matrix = torch.column_stack((in_feature, out_feature, self.weight_feature, gradients))            
             # # tracer
             # logger.info(f'{in_feature.shape} | {out_feature.shape} | {self.weight_feature.shape} | {gradients.shape}')
             # logger.info(graph_feature_matrix.shape)

eval.py
@@ -9,6 +9,7 @@ from typing import Any, Dict, List, Tuple, Union
 
 import matplotlib.pyplot as plt
 import torch
+import torch.nn.functional as F
 from loguru import logger
 from omegaconf import OmegaConf
 from torch import Tensor
@@ -19,7 +20,6 @@ from torchvision.transforms import ToTensor
 from data import GraphGenerator
 from model import HookedMNISTClassifier, MaskingGCN
 from trainer import gumbel_top_k_sampling_v2
-import torch.nn.functional as F
 
 global_config = OmegaConf.load("configs/config.yaml")
 
@@ -208,12 +208,12 @@ class Eval:
             score = round(100*score, 1) # percentage
 
         eval_set = 'forget set' if is_forget_set else 'retain set'
-        logger.info(f'Exp: {description} | Test loss on {eval_set} {test_loss} | Score {score} %')
+        logger.info(f'Exp: {description} | test_loss on {eval_set} {test_loss} | Score {score} %')
 
         metrics =  {
             'experiment': description,
             'eval data': eval_set,
-            'test loss': test_loss,
+            'test_loss': test_loss,
             'score': score,
         }
 
@@ -229,7 +229,7 @@ class Eval:
 
         # Plot loss
         axs[0].bar(categories, loss, color='blue')
-        axs[0].set_title('Test Loss')
+        axs[0].set_title('test_loss')
         axs[0].set_ylabel('Loss')
 
         # Plot score
@@ -241,7 +241,9 @@ class Eval:
         plt.tight_layout()
 
         self.config.metrics_path.mkdir(parents=True, exist_ok=True)
-        save_path = self.config.metrics_path / f'{experiment}_top_{self.config.topK}.png'
+        save_path = self.config.metrics_path / f'{experiment}/'
+        save_path.mkdir(parents=True, exist_ok=True)
+        save_path = save_path / f'top_{self.config.topK}.png'
         plt.savefig(save_path)
         plt.show()
 
@@ -274,7 +276,7 @@ class Eval:
         if self.draw_eval_plots:
 
             self.draw_visualization(
-                loss=[before_masking_eval_metrics['test loss'], after_masking_eval_metrics['test loss'], random_baseline_eval_metrics['test loss']],
+                loss=[before_masking_eval_metrics['test_loss'], after_masking_eval_metrics['test_loss'], random_baseline_eval_metrics['test_loss']],
                 score=[before_masking_eval_metrics['score'], after_masking_eval_metrics['score'], random_baseline_eval_metrics['score']],
                 experiment='eval_unlearning_on_forget_set'
             )
@@ -283,10 +285,20 @@ class Eval:
             'experiment': 'eval_unlearning',
             # 'top-K value': self.config.topK,
             'eval data': 'forget_set',
-            'before masking loss': before_masking_eval_metrics['test loss'],
-            'after masking loss': after_masking_eval_metrics['test loss'],
-            'before masking score': before_masking_eval_metrics['score'],
-            'after masking score': after_masking_eval_metrics['score']
+
+            'before_masking_loss': before_masking_eval_metrics['test_loss'],
+            'before_masking_score': before_masking_eval_metrics['score'],
+            'before_mask_probability': before_masking_eval_metrics['mean_classifier_probability_on_forget_digit'],
+
+            'after_masking_loss': after_masking_eval_metrics['test_loss'],
+            'after_masking_score': after_masking_eval_metrics['score'],
+            'after_mask_probability': after_masking_eval_metrics['mean_classifier_probability_on_forget_digit'],
+
+            'random_masking_loss': random_baseline_eval_metrics['test_loss'],
+            'random_masking_score': random_baseline_eval_metrics['score'],
+            'random_masking_probability': random_baseline_eval_metrics['mean_classifier_probability_on_forget_digit'],
+
+
         }
 
     def eval_performance_degradation(self) -> Dict:
@@ -318,7 +330,7 @@ class Eval:
         if self.draw_eval_plots:
 
             self.draw_visualization(
-                loss=[before_masking_eval_metrics['test loss'], after_masking_eval_metrics['test loss'], random_baseline_eval_metrics['test loss']],
+                loss=[before_masking_eval_metrics['test_loss'], after_masking_eval_metrics['test_loss'], random_baseline_eval_metrics['test_loss']],
                 score=[before_masking_eval_metrics['score'], after_masking_eval_metrics['score'], random_baseline_eval_metrics['score']],
                 experiment='eval_performance_degradation_on_retain_set'
             )
@@ -327,10 +339,15 @@ class Eval:
             'experiment': 'eval_performance_degradation',
             # 'top-K value': self.config.topK,
             'eval data': 'retain_set',
-            'before masking loss': before_masking_eval_metrics['test loss'],
-            'after masking loss': after_masking_eval_metrics['test loss'],
-            'before masking score': before_masking_eval_metrics['score'],
-            'after masking score': after_masking_eval_metrics['score']
+
+            'before_masking_loss': before_masking_eval_metrics['test_loss'],
+            'before_masking_score': before_masking_eval_metrics['score'],
+
+            'after_masking_loss': after_masking_eval_metrics['test_loss'],
+            'after_masking_score': after_masking_eval_metrics['score'],
+
+            'random_masking_loss': random_baseline_eval_metrics['test_loss'],
+            'random_masking_score': random_baseline_eval_metrics['score'],
         }
 
     def eval_mask_efficacy(self) -> Dict:
@@ -362,7 +379,7 @@ class Eval:
         if self.draw_eval_plots:
 
             self.draw_visualization(
-                loss=[before_masking_eval_metrics['test loss'], after_masking_eval_metrics['test loss'], random_baseline_eval_metrics['test loss']],
+                loss=[before_masking_eval_metrics['test_loss'], after_masking_eval_metrics['test_loss'], random_baseline_eval_metrics['test_loss']],
                 score=[before_masking_eval_metrics['score'], after_masking_eval_metrics['score'], random_baseline_eval_metrics['score']],
                 experiment='eval_mask_efficacy_on_foget_set'
             )
@@ -371,10 +388,15 @@ class Eval:
             'experiment': 'eval_performance_degradation',
             # 'top-K value': self.config.topK,
             'eval data': 'forget_set',
-            'before masking loss': before_masking_eval_metrics['test loss'],
-            'after masking loss': after_masking_eval_metrics['test loss'],
-            'before masking score': before_masking_eval_metrics['score'],
-            'after masking score': after_masking_eval_metrics['score']
+
+            'before_masking_loss': before_masking_eval_metrics['test_loss'],
+            'before_masking_score': before_masking_eval_metrics['score'],
+            
+            'after_masking_loss': after_masking_eval_metrics['test_loss'],
+            'after_masking_score': after_masking_eval_metrics['score'],
+
+            'random_masking_loss': random_baseline_eval_metrics['test_loss'],
+            'random_masking_score': random_baseline_eval_metrics['score'],
         }
 
     
@@ -395,7 +417,9 @@ class Eval:
         }
         if save_metrics:
             self.config.metrics_path.mkdir(exist_ok=True, parents=True)
-            file_path = self.config.metrics_path / f'top-{self.config.topK}.jsonl'
+            file_path = self.config.metrics_path / 'metrics'
+            file_path.mkdir(exist_ok=True, parents=True)
+            file_path = file_path / f'top-{self.config.topK}.json'
             with open(file_path, 'a') as f:
                 f.write(json.dumps(metrics) + '\n')
 

model.py
@@ -178,7 +178,7 @@ class MaskingGCNConv(MessagePassing):
 
 class MaskingGCN(nn.Module):
 
-    def __init__(self, num_node_features=4, num_message_passing_rounds=3, hidden_dim=32, use_deg_avg=False, output_logits=True):
+    def __init__(self, num_node_features=4, num_message_passing_rounds=3, hidden_dim=32, use_deg_avg=False, output_logits=True, use_relu=False):
         """
         Current node features:
             1) weight
@@ -198,6 +198,7 @@ class MaskingGCN(nn.Module):
         super().__init__()
 
         self.output_logits = output_logits
+        self.use_relu = use_relu
 
         self.proj_in = MaskingGCNConv(num_node_features, hidden_dim)
 
@@ -220,7 +221,11 @@ class MaskingGCN(nn.Module):
 
         x = self.proj_in(x, edge_index)
         for conv_layer in self.conv.values():
-            x = F.softmax(conv_layer(x, edge_index), dim=1)
+            x = conv_layer(x, edge_index)
+            if self.use_relu:
+                x = F.relu(x)
+            else:
+                x = F.sigmoid(x)
 
         if self.output_logits:
             return self.proj_out(x).squeeze()

trainer.py
@@ -87,7 +87,7 @@ class Trainer:
                 adam_optimizer.zero_grad()
 
                 if j % self.config.logging_steps == 0:
-                    logger.info(f'Epoch {i+1} | Step {j} | Loss {round(train_loss.item(), 2)}')
+                    logger.info(f'Epoch {i+1} | Step {j} | Classifier loss {round(train_loss.item(), 2)}')
                     self.val()
                     self.model.train()
 
@@ -144,7 +144,7 @@ class GCNTrainerConfig:
     gcn_checkpoint_path: Path = Path('checkpoints/gcn')
     lr=0.01
     weight_decay=5e-4
-    steps=32
+    steps=128
     mask_layer: Union[None, int] = -2
     mask_K: Union[int, Percentage] = 2_500 # number of parameters to keep or some Percentage of the model/layer 
     device = global_config.device
@@ -271,7 +271,9 @@ class GCNTrainer:
 
     def get_prior_distribution(self) -> Categorical:
         assert self.weight_vector is not None
-        probs = torch.abs(self.weight_vector) / torch.linalg.vector_norm(self.weight_vector, ord=1)
+        weight_vector = self.weight_vector.detach().clone()
+        assert not weight_vector.requires_grad
+        probs = torch.abs(weight_vector) / torch.linalg.vector_norm(weight_vector, ord=1)
         probs = probs.to(self.device)
         return Categorical(probs=probs)
     
@@ -315,12 +317,11 @@ class GCNTrainer:
             
             loss /= batch_size  # prevent exploding gradients while optimizing same objective
 
-            # 2x check retain_graph setting
-            loss.backward(retain_graph=True)
+            loss.backward()
             adam_optimizer.step()
             adam_optimizer.zero_grad()
             if s % 10 == 0:
-                logger.info(f'Step {s} | loss {loss.item()}')
+                logger.info(f'Step {s} | GCN loss {loss.item()}')
         
         ckpt_path = self.checkpoint()
         logger.info(f'GCN checkpoint saved at {ckpt_path}')